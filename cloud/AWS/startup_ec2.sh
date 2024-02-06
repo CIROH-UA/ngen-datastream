a37ba7fe5dd217110cdfd0635570e5ae993509f4
Jordan Laser <jordan.science.laser@gmail.com>
2024-02-06 07:29:30 -0700

    cloud updates

--- cloud/AWS/startup_ec2.sh
+++ cloud/AWS/startup_ec2.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+arch=$(dpkg --print-architecture)
+if [ "$arch" == "amd64" ]; then
+    curl -L -O https://github.com/LynkerIntel/hfsubset/releases/download/hfsubset-release-12/hfsubset-linux_amd64.tar.gz
+    curl -L -O https://s3.amazonaws.com/mountpoint-s3-release/latest/x86_64/mount-s3.rpm
+else
+    echo "no release for hfsubset on arm64"
+    curl -L -O https://s3.amazonaws.com/mountpoint-s3-release/latest/arm64/mount-s3.rpm
+fi
 sudo dnf update -y
 sudo dnf install git pip pigz awscli -y
-curl -L -O https://github.com/LynkerIntel/hfsubset/releases/download/hfsubset-release-12/hfsubset-linux_amd64.tar.gz
 tar -xzvf hfsubset-linux_amd64.tar.gz
+rm hfsubset-linux_amd64.tar.gz
 sudo mv hfsubset /usr/bin/hfsubset
 git clone https://github.com/CIROH-UA/ngen-datastream.git
 python3 -m pip install --upgrade pip
@@ -16,7 +24,8 @@ sudo dnf -y install dnf-plugins-core
 sudo dnf install docker -y
 sudo systemctl start docker
 sudo usermod -aG docker ${USER}
-curl -L -O https://s3.amazonaws.com/mountpoint-s3-release/latest/x86_64/mount-s3.rpm
+mkdir -p /home/ec2-user/ngen-datastream/data/mount
+
 sudo yum install -y ./mount-s3.rpm
 echo "cd docker && sudo docker build -t awiciroh/ngen-deps:latest -f Dockerfile.ngen-deps --no-cache . && docker build -t awiciroh/t-route:latest -f Dockerfile.t-route . --no-cache && docker build -t awiciroh/ngen -f Dockerfile.ngen . --no-cache && docker build -t awiciroh/ciroh-ngen-image:latest-local -f Dockerfile . --no-cache"
 echo "copy that ^^ and log out of session, log back in and run that command"
