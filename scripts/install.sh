338e808d02bfcfbaadeb8090f90d5fdad81017de
Jordan Laser <jordan.science.laser@gmail.com>
2024-03-01 10:09:46 -0700

    enable docker build on arm

---
+++ scripts/install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+USER=$(whoami)
+ARCH=$(uname -m)
+if [ "$ARCH" = "x86_64" ]; then \
+    curl -L -O https://s3.amazonaws.com/mountpoint-s3-release/latest/x86_64/mount-s3.rpm
+    PKG_MNGR="dnf"
+    sudo $PKG_MNGR update -y
+    sudo $PKG_MNGR install dnf-plugins-core -y
+elif [ "$ARCH" = "aarch64" ]; then \
+    curl -L -O https://s3.amazonaws.com/mountpoint-s3-release/latest/arm64/mount-s3.rpm
+    PKG_MNGR="yum"
+else \
+    echo "Unsupported architecture: $ARCH"; \
+    exit 1; \
+fi
+curl -L -O https://github.com/LynkerIntel/hfsubset/releases/download/hfsubset-release-12/hfsubset-linux_amd64.tar.gz
+sudo $PKG_MNGR update -y
+sudo $PKG_MNGR install ./mount-s3.rpm git pip pigz awscli -y
+tar -xzvf hfsubset-linux_amd64.tar.gz
+rm hfsubset-linux_amd64.tar.gz mount-s3.rpm
+sudo mv hfsubset /usr/bin/hfsubset
+git clone https://github.com/CIROH-UA/ngen-datastream.git
+sudo $PKG_MNGR install docker -y
+sudo systemctl start docker
+sudo usermod -aG docker ec2-user
+sudo newgrp docker
+su $USER
+"~/ngen-datastream/scripts/docker_builds.sh"
+
