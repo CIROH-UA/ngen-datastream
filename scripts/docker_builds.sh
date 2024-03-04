89912089149e239518cf9c261ddeba0b39af5b65
Jordan Laser <jordan.science.laser@gmail.com>
2024-03-04 09:50:33 -0700

    update docker builds

--- scripts/docker_builds.sh
+++ scripts/docker_builds.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+usage() {
+    echo ""
+    echo "Usage: $0 [options]"
+    echo "  -d, --DATASTREAM_PATH  <Path to ngen-datastream> "  
+}
+DATASTREAM_PATH=""
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+        -d|--DATASTREAM_PATH) DATASTREAM_PATH="$2"; shift 2;;
+        *) usage;;
+    esac
+done        
 
-DATASTREAM_PATH="$(eval echo ~$USER)/ngen-datastream"
 DATASTREAM_DOCKER="$DATASTREAM_PATH"/docker
-
+ARCH=$(uname -m)
+echo $ARCH
 cd $DATASTREAM_DOCKER
-docker build -t datastream-deps:latest -f Dockerfile.datastream-deps . --no-cache --build-arg TAG_NAME=latest && \
+docker build -t datastream-deps:latest -f Dockerfile.datastream-deps . --no-cache --build-arg TAG_NAME=latest --build-arg ARCH=$ARCH && \
 docker build -t forcingprocessor:latest -f Dockerfile.forcingprocessor . --no-cache --build-arg TAG_NAME=latest && \
     docker build -t validator:latest -f Dockerfile.validator . --no-cache --build-arg TAG_NAME=latest
 
