a7a726fbec419c3876cb101b0a729a7c799dd125
Jordan Laser <jordan.science.laser@gmail.com>
2024-03-06 10:26:50 -0700

    implement windowing

--- forcingprocessor/src/forcingprocessor/weights_parq2json.py
+++ forcingprocessor/src/forcingprocessor/weights_parq2json.py
@@ -1,5 +1,6 @@
 import concurrent.futures as cf
 import json
+import numpy as np
 import os, argparse, time
 import geopandas as gpd
 gpd.options.io_engine = "pyogrio"
@@ -9,6 +10,8 @@ import pyarrow.dataset
 
 def get_weight_json(catchments,version=None):
     if version is None: version = "v20.1"
+
+    data_shp = (3840,4608)
     
     weight_data = {}
     print(f'Beginning weights query')
@@ -20,6 +23,8 @@ def get_weight_json(catchments,version=None):
     batch: pa.RecordBatch
     ncatch_found = 0
     t_weights = time.perf_counter()    
+    [x_min, x_max] = [10000,0]
+    [y_min, y_max] = [10000,0]
     for batch in w:
         tbl = batch.to_pandas()
         if tbl.empty:
@@ -30,10 +35,22 @@ def get_weight_json(catchments,version=None):
             if df_jcatch.empty:
                 continue  
             ncatch_found+=1
-            weight_data[jcatch] = [[int(x) for x in list(df_jcatch['cell'])],list(df_jcatch['coverage_fraction'])]
+            idx_list = [int(x) for x in list(df_jcatch['cell'])]
+            df_catch = list(df_jcatch['coverage_fraction'])
+            weight_data[jcatch] = [idx_list,df_catch]
+            [x,y] = np.unravel_index(idx_list,(data_shp[0], data_shp[1]))
+            jxmin = min(x)
+            jxmax = max(x)
+            jymin = min(y)
+            jymax = max(y)
+            if jxmin < x_min: x_min = jxmin
+            if jxmax > x_max: x_max = jxmax
+            if jymin < y_min: y_min = jymin
+            if jymax > y_max: y_max = jymax
+    
     print(f'Weights calculated for {ncatch_found} catchments in {time.perf_counter() - t_weights:.1f} seconds')
 
-    return (weight_data)
+    return (weight_data, int(x_min), int(x_max), int(y_min), int(y_max))
 
 def get_catchments_from_gpkg(gpkg):
     catchments     = gpd.read_file(gpkg, layer='divides')
@@ -45,9 +62,10 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--gpkg', dest="geopackage", type=str, help="Path to geopackage file",default = None)
+    parser.add_argument('--catchment_list', dest="catchment_list", type=str, help="list of catchments",default = None)
     parser.add_argument('--outname', dest="weights_filename", type=str, help="Filename for the weight file")
     parser.add_argument('--version', dest="version", type=str, help="Hydrofabric version e.g. \"v21\"",default = None)
-    parser.add_argument('--nprocs', dest="nprocs_max", type=int, help="Maximum number of processes")
+    parser.add_argument('--nprocs', dest="nprocs_max", type=int, help="Maximum number of processes",default=1)
     args = parser.parse_args()
 
     version = args.version    
@@ -55,35 +73,22 @@ if __name__ == "__main__":
     if version not in weight_versions: 
         raise Exception(f'version must one of: {weight_versions}')
 
-    if args.geopackage is None:
-        # go for conus
-        import pandas as pd
-        uri = f"s3://lynker-spatial/{version}/forcing_weights.parquet"
-        weights_df = pd.read_parquet(uri)
-        catchment_list = list(weights_df.divide_id.unique())
-        del weights_df
+    if args.catchment_list is not None:
+        catchment_list = args.catchment_list.split(',')
     else:
-        catchment_list = get_catchments_from_gpkg(args.geopackage)
+        if args.geopackage is None:
+            # go for conus
+            import pandas as pd
+            uri = f"s3://lynker-spatial/{version}/forcing_weights.parquet"
+            weights_df = pd.read_parquet(uri) 
+            catchment_list = list(weights_df.divide_id.unique())
+            del weights_df
+        else:
+            catchment_list = get_catchments_from_gpkg(args.geopackage)
 
-    ncatchments = len(catchment_list)
-    nprocs = min([os.cpu_count() // 5, args.nprocs_max,ncatchments])
-    print(f'Querying weights with {nprocs} processes')
-    catchment_list_list = []    
-    nper = ncatchments // nprocs
-    nleft = ncatchments - (nper * nprocs)
-    i = 0
-    k = 0
-    for _ in range(nprocs):
-        k = nper + i + nleft      
-        catchment_list_list.append(catchment_list[i:k])
-        i = k
-    
-    with cf.ProcessPoolExecutor(max_workers=nprocs) as pool:
-        results = pool.map(get_weight_json,catchment_list_list)
+    (weights, x_min, x_max, y_min, y_max) = get_weight_json(catchment_list)
 
-    weights = {}
-    for jweights in results:
-        weights = weights | jweights
+    weights['window'] = [x_min,x_max,y_min,y_max]
 
     data = json.dumps(weights)
     with open(args.weights_filename,'w') as fp:
