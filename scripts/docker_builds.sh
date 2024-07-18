5a2174dacc455ebb92ec648ce2e8465b272e159f
Jordan Laser <jlaser@lynker.com>
2024-07-17 20:07:28 -0600

    Update docker_builds.sh

--- scripts/docker_builds.sh
+++ scripts/docker_builds.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-DATASTREAM_PATH="$(dirname "$SCRIPT_DIR")"       
+DATASTREAM_PATH="$(dirname "$SCRIPT_DIR")"
 PLATFORM=$(uname -m)
 PLATORM_TAG=""
 if [ $PLATFORM = "x86_64" ]; then
@@ -22,15 +22,15 @@ TAG="latest$PLATORM_TAG"
 DATASTREAM_DOCKER="$DATASTREAM_PATH"/docker
 cd $DATASTREAM_DOCKER
 echo "Building docker from "$DATASTREAM_DOCKER
-docker build -t awiciroh/datastream-deps:$TAG -f Dockerfile.datastream-deps . --no-cache --build-arg TAG_NAME=$TAG --build-arg ARCH=$PLATFORM --platform linux/$PLATFORM 
+docker build -t awiciroh/datastream-deps:$TAG -f Dockerfile.datastream-deps . --no-cache --build-arg TAG_NAME=$TAG --build-arg ARCH=$PLATFORM --platform linux/$PLATFORM
 docker build -t awiciroh/forcingprocessor:$TAG -f Dockerfile.forcingprocessor . --no-cache --build-arg TAG_NAME=$TAG --platform linux/$PLATFORM
 docker build -t awiciroh/datastream:$TAG -f Dockerfile.datastream . --no-cache --build-arg TAG_NAME=$TAG --platform linux/$PLATFORM
 
 if [ "$DOCKER_PUSH" = "yes" ]; then
     echo "Pushing docker containers"
-    docker push awiciroh/datastream-deps:latest-x86
-    docker push awiciroh/datastream:latest-x86
-    docker push awiciroh/forcingprocessor:latest-x86
+    docker push awiciroh/datastream-deps:latest$PLATORM_TAG
+    docker push awiciroh/datastream:latest$PLATORM_TAG
+    docker push awiciroh/forcingprocessor:latest$PLATORM_TAG
 fi
 
-echo "done!"    
+echo "done!"
