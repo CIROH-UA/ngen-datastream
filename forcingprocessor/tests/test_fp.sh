ac340f167c8b22599d1ce1b8a7ce09602360c528
Jordan Laser <jordan.science.laser@gmail.com>
2024-09-03 12:11:49 -0600

    move shell to yml

--- forcingprocessor/tests/test_fp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-python -m pytest -vv --deselect="forcingprocessor/tests/test_forcingprocessor.py::test_google_cloud_storage" --deselect="forcingprocessor/tests/test_forcingprocessor.py::test_gcs" --deselect="forcingprocessor/tests/test_forcingprocessor.py::test_gs" --deselect="forcingprocessor/tests/test_forcingprocessor.py::test_ciroh_zarr" --deselect="forcingprocessor/tests/test_forcingprocessor.py::test_nomads_post_processed" --deselect="forcingprocessor/tests/test_forcingprocessor.py::test_retro_ciroh_zarr"
-python -m pytest -v -k test_google_cloud_storage 
-python -m pytest -v -k test_gs 
-python -m pytest -v -k test_gcs 
