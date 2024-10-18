d7df2a9f4a9bc3303d36d727643bcd03f6168b2c
Jordan Laser <jlaser@lynker.com>
2024-10-18 10:34:53 -0600

    Update configure_datastream.py
    Remove $

--- python_tools/src/python_tools/configure_datastream.py
+++ python_tools/src/python_tools/configure_datastream.py
@@ -178,7 +178,7 @@ def create_conf_fp(args):
     output_file_type = ["netcdf"]
     if len(args.s3_bucket) > 0:
         if "DAILY" in args.start_date: 
-            args.s3_prefix = re.sub(r"\$DAILY",datetime.now(tz.timezone('US/Eastern')).strftime('%Y%m%d'),args.s3_prefix)
+            args.s3_prefix = re.sub(r"\DAILY",datetime.now(tz.timezone('US/Eastern')).strftime('%Y%m%d'),args.s3_prefix)
         output_path  = f"s3://{args.s3_bucket}/{args.s3_prefix}"
     elif len(args.docker_mount) > 0:
         gpkg_file = [f"{args.docker_mount}/datastream-resources/config/{geo_base}"]
