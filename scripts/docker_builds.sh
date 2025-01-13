01f52d91478a1027121a2ff344386cae16f1df9e
Jordan Laser <jordan.science.laser@gmail.com>
2025-01-13 11:57:15 -0700

    build, test, and push all on one runner

--- scripts/docker_builds.sh
+++ scripts/docker_builds.sh
@@ -6,24 +6,24 @@ DOCKER_DATASTREAM=$DOCKER_DIR/ngen-datastream
 
 BUILD_FORCINGPROCESSOR="no"
 BUILD_DATASTREAM="no"
-PUSH="no"
 BUILD_DEPS="no"
-while getopts "pefd" flag; do
- case $flag in
-   p) PUSH="yes"
-   ;;
-   e) BUILD_DEPS="yes"
-   ;; 
-   f) BUILD_FORCINGPROCESSOR="yes"
-   ;; 
-   d) BUILD_DATASTREAM="yes"
-   ;;         
-   \?)
-   ;;
- esac
+TAG="tmp"
+while getopts "pefdt:" flag; do
+  case $flag in
+    e) BUILD_DEPS="yes"
+    ;;
+    f) BUILD_FORCINGPROCESSOR="yes"
+    ;;
+    d) BUILD_DATASTREAM="yes"
+    ;;
+    t) TAG="$OPTARG"
+    ;;
+    \?)
+    echo "Invalid option: -$OPTARG" >&2
+    ;;
+  esac
 done
 
-TAG="latest"
 cd $DOCKER_DIR
 if [ "$BUILD_DEPS" = "yes" ]; then
   docker buildx build -t awiciroh/datastream-deps:$TAG -f Dockerfile.datastream-deps . --no-cache --platform linux/amd64,linux/arm64
@@ -44,26 +44,10 @@ if [ "$BUILD_DATASTREAM" = "yes" ]; then
   cp -r $DATASTREAM_PATH/python_tools $DOCKER_DATASTREAM/python_tools
   cp -r $DATASTREAM_PATH/configs $DOCKER_DATASTREAM/configs
 
-  NGEN_CAL_DOCKER=$DOCKER_DIR/ngen-cal
-  cp -r $DATASTREAM_PATH/ngen-cal $DOCKER_DIR
-
   docker buildx build -t awiciroh/datastream:$TAG -f Dockerfile.datastream . --no-cache --platform linux/amd64,linux/arm64
   if [ -d "$DOCKER_DATASTREAM" ]; then
     rm -rf $DOCKER_DATASTREAM
   fi
-  if [ -d "$NGEN_CAL_DOCKER" ]; then
-    rm -rf $NGEN_CAL_DOCKER
-  fi
-fi
-
-if [ "$PUSH" = "yes" ]; then
-    echo "Pushing docker containers"
-    if [ "$SKIP_DEPS" = "no" ]; then
-      docker push awiciroh/datastream-deps:$TAG
-    fi
-    docker push awiciroh/datastream:$TAG
-    docker push awiciroh/forcingprocessor:$TAG
-    echo "Docker containers have been pushed to awiciroh dockerhub!"
 fi
 
 
