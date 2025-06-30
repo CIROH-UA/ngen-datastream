5f6966b157c2e97bb81fe93e4b92ba736010693b
Jordan Laser <jordan.science.laser@gmail.com>
2025-06-30 08:36:02 -0600

    manifest instead of buildx

--- scripts/docker_builds.sh
+++ scripts/docker_builds.sh
@@ -4,64 +4,66 @@ DATASTREAM_PATH="$(dirname "$SCRIPT_DIR")"
 DOCKER_DIR="$DATASTREAM_PATH"/docker
 DOCKER_DATASTREAM=$DOCKER_DIR/ngen-datastream
 
+PLATFORM=$(uname -m)
+PLATORM_TAG="-arm64"
+if [ $PLATFORM = "x86_64" ]; then
+    PLATORM_TAG="-x86"
+fi
 TAG="latest"
-PLATFORMS="linux/amd64,linux/arm64"
 
 BUILD_FORCINGPROCESSOR="no"
 BUILD_DATASTREAM="no"
 PUSH="no"
 BUILD_DEPS="no"
-while getopts "pefd" flag; do
+while getopts "pefdt:" flag; do
  case $flag in
    p) PUSH="yes"
    ;;
    e) BUILD_DEPS="yes"
-   ;; 
+   ;;
    f) BUILD_FORCINGPROCESSOR="yes"
-   ;; 
+   ;;
    d) BUILD_DATASTREAM="yes"
-   ;;         
+   ;;
+   t) TAG="$OPTARG"
+   ;;
    \?)
    ;;
  esac
 done
 
 cd $DOCKER_DIR
-
-if ! docker buildx ls | grep -q 'multi-builder'; then
-  docker buildx create --name multi-builder --use
-else
-  docker buildx use multi-builder
-fi
-
 if [ "$BUILD_DEPS" = "yes" ]; then
-  docker buildx build --platform $PLATFORMS -t awiciroh/datastream-deps:$TAG -f Dockerfile.datastream-deps . --no-cache --build-arg ARCH=multi --push=$([ "$PUSH" = "yes" ] && echo true || echo false)
+  docker build -t awiciroh/datastream-deps:$TAG -f Dockerfile.datastream-deps . --no-cache --build-arg ARCH=$PLATFORM --platform linux/$PLATFORM
   if [ -d "$DOCKER_DATASTREAM" ]; then
     rm -rf $DOCKER_DATASTREAM
   fi
 fi
-
 if [ "$BUILD_FORCINGPROCESSOR" = "yes" ]; then
   mkdir $DOCKER_DATASTREAM
   cp -r $DATASTREAM_PATH/forcingprocessor $DOCKER_DATASTREAM/forcingprocessor
-  docker buildx build --platform $PLATFORMS -t awiciroh/forcingprocessor:$TAG -f Dockerfile.forcingprocessor . --no-cache --build-arg TAG_NAME=$TAG --push=$([ "$PUSH" = "yes" ] && echo true || echo false)
+  docker build -t awiciroh/forcingprocessor:$TAG -f Dockerfile.forcingprocessor . --no-cache --build-arg TAG_NAME=$TAG --platform linux/$PLATFORM
   if [ -d "$DOCKER_DATASTREAM" ]; then
     rm -rf $DOCKER_DATASTREAM
   fi
 fi
-
 if [ "$BUILD_DATASTREAM" = "yes" ]; then
   mkdir $DOCKER_DATASTREAM
   cp -r $DATASTREAM_PATH/python_tools $DOCKER_DATASTREAM/python_tools
   cp -r $DATASTREAM_PATH/configs $DOCKER_DATASTREAM/configs
-  docker buildx build --platform $PLATFORMS -t awiciroh/datastream:$TAG -f Dockerfile.datastream . --no-cache --build-arg TAG_NAME=$TAG --push=$([ "$PUSH" = "yes" ] && echo true || echo false)
+  docker build -t awiciroh/datastream:$TAG -f Dockerfile.datastream . --no-cache --build-arg TAG_NAME=$TAG --platform linux/$PLATFORM
   if [ -d "$DOCKER_DATASTREAM" ]; then
     rm -rf $DOCKER_DATASTREAM
   fi
 fi
 
 if [ "$PUSH" = "yes" ]; then
+    echo "Pushing docker containers"
+    if [ "$SKIP_DEPS" = "no" ]; then
+      docker push awiciroh/datastream-deps:$TAG
+    fi
+    docker push awiciroh/datastream-deps:$TAG
+    docker push awiciroh/datastream:$TAG
+    docker push awiciroh/forcingprocessor:$TAG
     echo "Docker containers have been pushed to awiciroh dockerhub!"
-else
-    echo "Build complete. Use -p to push images to Docker Hub."
 fi
