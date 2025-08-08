848c694e3361c3b14adb63bdc17809b7d0dafe8b
harshavemula-ua <svemula1@ua.edu>
2025-08-08 10:54:03 -0500

    Update docker_loginNpush.sh

--- research_datastream/terraform/test/docker_loginNpush.sh
+++ research_datastream/terraform/test/docker_loginNpush.sh
@@ -5,7 +5,7 @@ DOCKERHUB_USERNAME="awiciroh"
 
 if [ "$(echo $DOCKERHUB_TOKEN | docker login -u $DOCKERHUB_USERNAME --password-stdin)" == "Login Succeeded" ]; then             
     echo "Docker login successful"
-    /home/ec2-user/ngen-datastream/scripts/docker_builds.sh -p -t latest-arm64 >> /home/ec2-user/ngen-datastream/docker_build_log.txt
+    /home/ec2-user/ngen-datastream/scripts/docker_builds.sh -p -a -t latest-arm64 >> /home/ec2-user/ngen-datastream/docker_build_log.txt
     echo "Push complete"
 else
     echo "Docker login failed"
