587aa3de1ecbd53cb9f1f16a54846220ebebff30
harshavemula-ua <svemula1@ua.edu>
2025-08-20 18:06:03 -0500

    Update docker_loginNpush.sh

--- research_datastream/terraform/test/docker_loginNpush.sh
+++ research_datastream/terraform/test/docker_loginNpush.sh
@@ -7,9 +7,16 @@ DOCKERHUB_TOKEN="$(aws secretsmanager get-secret-value --secret-id docker_awicir
 DOCKERHUB_USERNAME="awiciroh"
 
 if [ "$(echo $DOCKERHUB_TOKEN | docker login -u $DOCKERHUB_USERNAME --password-stdin)" == "Login Succeeded" ]; then             
-    echo "Docker login successful"
-    /home/ec2-user/ngen-datastream/scripts/docker_builds.sh -p -t "$TAG" >> /home/ec2-user/ngen-datastream/docker_build_log.txt
-    echo "Push complete"
+    echo "Docker login successful"    
+    echo "Retagging and pushing images with tag: $TAG"
+    # Retag and push datastream-deps
+    docker tag awiciroh/datastream-deps:latest-arm64 awiciroh/datastream-deps:$TAG
+    # Retag and push datastream
+    docker tag awiciroh/datastream:latest-arm64 awiciroh/datastream:$TAG
+    # Retag and push forcingprocessor
+    docker tag awiciroh/forcingprocessor:latest-arm64 awiciroh/forcingprocessor:$TAG
+
+    /home/ec2-user/ngen-datastream/scripts/docker_builds.sh $BUILD_ARGS -p -t $TAG >> /home/ec2-user/ngen-datastream/docker_build_log.txt
 else
     echo "Docker login failed"
     exit 1
