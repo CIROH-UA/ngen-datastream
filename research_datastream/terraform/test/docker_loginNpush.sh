3db708d3e938bc7d3617db8dcba1b08da1d647f2
harshavemula-ua <svemula1@ua.edu>
2025-08-20 17:18:11 -0500

    Update docker_loginNpush.sh

--- research_datastream/terraform/test/docker_loginNpush.sh
+++ research_datastream/terraform/test/docker_loginNpush.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+# Accept tag as command line argument, default to "latest-arm64" if not provided
+TAG="${1:-latest-arm64}"
+
 DOCKERHUB_TOKEN="$(aws secretsmanager get-secret-value --secret-id docker_awiciroh_creds --region us-east-1 --query SecretString --output text | jq -r .DOCKERHUB_TOKEN)"
 DOCKERHUB_USERNAME="awiciroh"
 
 if [ "$(echo $DOCKERHUB_TOKEN | docker login -u $DOCKERHUB_USERNAME --password-stdin)" == "Login Succeeded" ]; then             
     echo "Docker login successful"
-    /home/ec2-user/ngen-datastream/scripts/docker_builds.sh buildtags -t latest-arm64 >> /home/ec2-user/ngen-datastream/docker_build_log.txt
-    /home/ec2-user/ngen-datastream/research_datastream/terraform/test/retag.sh buildtags DS_DP_Tag FP_Tag DS_Tag  >> /home/ec2-user/ngen-datastream/docker_build_log.txt
+    /home/ec2-user/ngen-datastream/scripts/docker_builds.sh -p -t "$TAG" >> /home/ec2-user/ngen-datastream/docker_build_log.txt
     echo "Push complete"
 else
     echo "Docker login failed"
     exit 1
 fi
-
